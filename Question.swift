c68618058b5033f7afb64edee50b540fabf9fadc
shaoying <shying@vt.edu>
2017-10-08 23:41:46 -0400

    Quizzer/

---
+++ Question.swift
@@ -0,0 +1,25 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Shaoying Ying on 10/1/17.
+//
+
+import Foundation
+
+class Question {
+   //Properties
+    let questionText : String
+    let answer : Bool
+    //Events
+    init(text:String,correctAnswer: Bool){
+        questionText = text
+        answer = correctAnswer
+    }
+    //Action/Methods
+    
+}
+
+//class myOtherClass {
+//    let question = Question(text:"What's the meaning of life",correctAnswer: true)
+//}
